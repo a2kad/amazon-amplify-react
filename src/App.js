f23466cede891c2ad111123c0f278e35eec0c897
a2kad <reshetnikovruslan705@gmail.com>
2023-04-12 20:57:55 +0200

    Changed text

--- src/App.js
+++ src/App.js
@@ -7,16 +7,9 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Le site personnel de <code>Ruslan RESHETNIKOV</code> sera ici.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        
       </header>
     </div>
   );
